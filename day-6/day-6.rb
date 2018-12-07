e8e6bd7b5bbb01fee084d84251dcbb8a223be512
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2018-12-06 16:23:40 -0800

    Update day-6.rb

--- day-6/day-6.rb
+++ day-6/day-6.rb
@@ -20,35 +20,27 @@
 
 def part_one(input)
   point_map = Array.new(400) {Array.new(400, '#')}
-  min_x, min_y = [1000, 1000]
-  max_x, max_y = [0, 0]
   points = []
   input.each do |pt|
     x, y = pt.split(', ')
     points.push([x.to_i,y.to_i])
   end
-  points.each do |x_pt, y_pt|
-    min_x = x_pt if min_x > x_pt
-    max_x = x_pt if max_x < x_pt
-    min_y = y_pt if min_y > y_pt
-    max_y = y_pt if max_y < y_pt
-  end
   (0..399).each do |y|
-    match = true
     (0..399).each do |x|
-      mdist = 100000
+      not_eq_distance = true
+      min_distance = 100000
       name = ''
       points.each do |p|
         distance = (p[0] - x).abs + (p[1] - y).abs
-        if distance < mdist
-          mdist = distance
+        if distance < min_distance
+          min_distance = distance
           name = points.index(p).to_s
-          match = false
-        elsif distance == mdist
-          match = true
+          not_eq_distance = true
+        elsif distance == min_distance
+          not_eq_distance = false
         end
       end
-      if not match
+      if not_eq_distance
         point_map[y][x] = name
       end
     end
@@ -63,7 +55,7 @@ def part_one(input)
   pts = Array.new(50, 0)
   (0..399).each do |y|
     (0..399).each do |x|
-      if point_map[y][x] != '*' && (not removed_points.include?(point_map[y][x]))
+      if (point_map[y][x] != '*') && (not removed_points.include?(point_map[y][x]))
         i = point_map[y][x].to_i
         pts[i] += 1
       end
@@ -80,57 +72,20 @@ end
 
 def part_two(input)
   point_map = Array.new(400) {Array.new(400, '#')}
-  min_x, min_y = [1000, 1000]
-  max_x, max_y = [0, 0]
   points = []
   input.each do |pt|
     x, y = pt.split(', ')
     points.push([x.to_i,y.to_i])
   end
-  points.each do |x_pt, y_pt|
-    min_x = x_pt if min_x > x_pt
-    max_x = x_pt if max_x < x_pt
-    min_y = y_pt if min_y > y_pt
-    max_y = y_pt if max_y < y_pt
-  end
   region_area = 0
   (0..399).each do |y|
-    match = true
     (0..399).each do |x|
-      mdist = 100000
       total_dist = 0
-      name = ''
       points.each do |p|
         distance = (p[0] - x).abs + (p[1] - y).abs
         total_dist += distance
-        if distance < mdist
-          mdist = distance
-          name = points.index(p).to_s
-          match = false
-        elsif distance == mdist
-          match = true
-        end
       end
       region_area += 1 if total_dist < 10000
-      if not match
-        point_map[y][x] = name
-      end
-    end
-  end
-  removed_points = []
-  (0..399).each do |axis|
-    removed_points.append(point_map[0][axis]) if not removed_points.include?(point_map[0][axis])
-    removed_points.append(point_map[axis][0]) if not removed_points.include?(point_map[axis][0])
-    removed_points.append(point_map[399][axis]) if not removed_points.include?(point_map[399][axis])
-    removed_points.append(point_map[axis][399]) if not removed_points.include?(point_map[axis][399])
-  end
-  pts = Array.new(50, 0)
-  (0..399).each do |y|
-    (0..399).each do |x|
-      if point_map[y][x] != '*' && (not removed_points.include?(point_map[y][x]))
-        i = point_map[y][x].to_i
-        pts[i] += 1
-      end
     end
   end
   puts region_area
