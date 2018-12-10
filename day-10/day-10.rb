11e8f19dc327020611404f28b3d09854b5cc1661
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2018-12-09 23:55:01 -0800

    Update day-10.rb

--- day-10/day-10.rb
+++ day-10/day-10.rb
@@ -62,33 +62,79 @@ def part_one(input)
     y0 += lights[i_max_y][:velocity][1]
     y1 += lights[i_min_y][:velocity][1]
     time_to_take += 1
-    break if (x1 + 100) > x0
+    box = (x0 - x1) * (y0 - y1)
+    break if box < 0
   end
-  1.upto(10357) do |j|
-    lights_map = Array.new(200) {Array.new(300, '.')} if j > 10352
+  1.upto(time_to_take) do |j|
+    lights_map = Array.new(200) {Array.new(300, '.')} if j == (time_to_take - 1)
     lights.each do |light|
       light[:position][0] += light[:velocity][0]
       light[:position][1] += light[:velocity][1]
       x_index = light[:position][0]
       y_index = light[:position][1]
-      lights_map[y_index][x_index] = '#' if j > 10352
+      lights_map[y_index][x_index] = '#' if j == (time_to_take - 1)
     end
-    if j > 10352
+    if j == (time_to_take - 1)
       parr = []
       lights_map.each do |ln|
         parr.push(ln.join(''))
       end
-      IO.write("map-#{j}.txt", parr.join("\n"))
+      IO.write('map.txt', parr.join("\n"))
     end
   end
 end
 
-# def part_two(input)
-# end
+def part_two(input)
+  lights = []
+  max_x = -1000000
+  min_x = 1000000
+  max_y = -1000000
+  min_y = 1000000
+  time_to_take = 0
+  i_max_x, i_min_x, i_max_y, i_min_y = 0, 0, 0, 0
+
+  input.each do |l|
+    line = l.gsub('=<', '=>[')
+    line.gsub!('> ', '], :')
+    line.gsub!(/>$/, ']}')
+    line.prepend('{:')
+    lights.push(eval line)
+  end
+  lights.each_with_index do |lgt, i|
+    if lgt[:position][0] > max_x
+      max_x = lgt[:position][0]
+      i_max_x = i
+    end
+    if lgt[:position][0] < min_x
+      min_x = lgt[:position][0]
+      i_min_x = i
+    end
+    if lgt[:position][1] > max_y
+      max_y = lgt[:position][1]
+      i_max_y = i
+    end
+    if lgt[:position][1] < min_y
+      min_y = lgt[:position][1]
+      i_min_y = i
+    end
+  end
+  x0 = lights[i_max_x][:position][0]
+  x1 = lights[i_min_x][:position][0]
+  y0 = lights[i_max_y][:position][1]
+  y1 = lights[i_min_y][:position][1]
+  loop do
+    x0 += lights[i_max_x][:velocity][0]
+    x1 += lights[i_min_x][:velocity][0]
+    y0 += lights[i_max_y][:velocity][1]
+    y1 += lights[i_min_y][:velocity][1]
+    time_to_take += 1
+    box = (x0 - x1) * (y0 - y1)
+    break if box < 0
+  end
+  puts time_to_take - 1
+end
 
 input_file = File.readlines('day-10-input.txt').map(&:chomp)
 
-# Hand iterated for part two
-
 part_one(input_file) # HRPHBRKG
-# part_two(input_file) # 10355
+part_two(input_file) # 10355
